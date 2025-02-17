f904fdcf8c7385056dc2b944a385ff015db881f6
shefat2002 <shefat2002@gmail.com>
2025-02-17 08:40:17 +0600

    Updated a few things

---
+++ script.js
@@ -0,0 +1,15 @@
+var togglebtn = document.querySelector('.togglebtn');
+var nav = document.querySelector('.navlinks');
+var links = document.querySelector('.navlinks li');
+
+togglebtn.addEventListener('click', function() {
+    this.classList.toggle('click');
+    nav.classList.toggle('open');
+});
+
+var typed = new Typed('.input', {
+    strings: ['Web Developer', 'Competitive Programmer', 'Programming Instructor',  'Muslim'],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop: true
+});
